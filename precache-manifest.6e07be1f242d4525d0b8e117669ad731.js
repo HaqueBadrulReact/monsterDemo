6d56ebf282abdf5bb712f92b0ce2e32a0ccc0210
BadrulHaqueReact <haquebadrulreactjs2020@gmail.com>
2020-05-11 10:22:39 +0530

    Updates

---
+++ precache-manifest.6e07be1f242d4525d0b8e117669ad731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005ede69a4fed6e566df6e13162ed15f",
+    "url": "/monsterDemo/index.html"
+  },
+  {
+    "revision": "5ca03a51cb98d44124e2",
+    "url": "/monsterDemo/static/css/main.7a732d4f.chunk.css"
+  },
+  {
+    "revision": "52ab36c179d2e860984b",
+    "url": "/monsterDemo/static/js/2.1662debb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterDemo/static/js/2.1662debb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca03a51cb98d44124e2",
+    "url": "/monsterDemo/static/js/main.4aafd0f7.chunk.js"
+  },
+  {
+    "revision": "6184f01888486a31ba48",
+    "url": "/monsterDemo/static/js/runtime-main.5dd7bbdf.js"
+  }
+]);
